51f27773e5fc0f216d28da7a9c2716384e8182c2
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-10-03 17:31:54 -0400

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,16 +2,25 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+const cards = document.querySelector('.cards');
+axios
+	.get('https://api.github.com/users/jasminekh96')
+	.then((response) => {
+		/* Step 4: Pass the data received from Github into your function, 
+           create a new component and add it to the DOM as a child of .cards*/
+		// const myInfo = response.response;
+		console.log(response.data);
+		const cardInfo = followers(response.data);
+		cards.appendChild(cardInfo);
+	})
+	.catch((error) => {
+		console.log('The data was not returned', error);
+	});
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
    Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
 */
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
@@ -24,7 +33,53 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [ 'JustinTrombley96', 'normabunton', 'MelodyRackham', 'HarrisonMS', 'nattroj' ];
+function followers(people) {
+	const followersCard = document.createElement('div'),
+		followersCardInfo = document.createElement('div'),
+		followersImage = document.createElement('img'),
+		followersName = document.createElement('h3'),
+		followersUser = document.createElement('username'),
+		followersLocation = document.createElement('p'),
+		followersProfile = document.createElement('p'),
+		followersFollowers = document.createElement('p'),
+		followersFollowing = document.createElement('p'),
+		followersBio = document.createElement('p');
+	//
+	followersImage.src = people.avatar_url;
+	followersName.textContent = people.name;
+	followersUser.textContent = people.login;
+	followersLocation.textContent = `Location: ${people.location}`;
+	followersProfile.textContent = people.html_url;
+	followersFollowers.textContent = `Followers: ${people.followers}`;
+	followersFollowing.textContent = `Following: ${people.following}`;
+	followersBio.textContent = `Bio: ${people.bio}`;
+	//
+	followersCard.classList.add('card');
+	followersCardInfo.classList.add('card-info');
+	followersName.classList.add('name');
+	followersUser.classList.add('username');
+	//
+	followersCard.appendChild(followersImage);
+	followersCard.appendChild(followersCardInfo);
+	followersCardInfo.appendChild(followersName);
+	followersCardInfo.appendChild(followersUser);
+	followersCardInfo.appendChild(followersLocation);
+	followersCardInfo.appendChild(followersProfile);
+	followersCardInfo.appendChild(followersFollowers);
+	followersCardInfo.appendChild(followersFollowing);
+	followersCardInfo.appendChild(followersBio);
+	//
+	return followersCard;
+}
+followersArray.forEach(function(user) {
+	axios.get(`https://api.github.com/users/${user}`).then((response) => {
+		const peoples = followers(response.data);
+		cards.appendChild(peoples);
+		console.log(response);
+		console.log(user);
+	});
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
